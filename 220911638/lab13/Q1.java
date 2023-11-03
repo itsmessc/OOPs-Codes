0342780a42f3ca2896a1b39c1e05d6bfbac30338
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2023-11-03 10:45:20 +0530

    Add files via upload

---
+++ 220911638/lab13/Q1.java
@@ -0,0 +1,26 @@
+package lab13;
+import javafx.application.Application;
+import javafx.scene.control.*;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+public class Q1 extends Application {
+    public void start(Stage primaryStage)
+    {
+        primaryStage.setTitle("This is the first javafx application");
+        Label l=new Label();
+        l.setText("Welcome to javafx programming");
+        l.setTextFill(Color.MAGENTA);
+        FlowPane root = new FlowPane(10 ,30);
+        root.getChildren().add(l);
+        Scene s=new Scene(root,500,200);
+        primaryStage.setScene(s);
+        primaryStage.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
