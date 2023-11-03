0cb3b2c6f1f3015f50c179218de97b6c4795df68
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2023-11-03 09:47:42 +0530

    Add files via upload

---
+++ 220911638/lab 11/Matrixthread.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+public class Matrixthread {
+    private int m[][];
+    private int row,column;
+    public Matrixthread(int r,int c){
+        Scanner sc=new Scanner(System.in);
+        row=r;
+        column=c;
+        m=new int[r][c];
+        System.out.println("Enter elements of matrix");
+        int i,j;
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                m[i][j]=sc.nextInt();
+            }
+        }
+    }
+    public void display()
+    {
+        System.out.println("Matrix is :");
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                System.out.print(m[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public void transpose()
+    {
+        System.out.println("Transpose is :");
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                System.out.print(m[j][i]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public void maxinmat()
+    {
+        int max=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                if(max<m[i][j]);
+                max=m[i][j];
+            }
+        }
+        System.out.println("Maximum="+max);
+    }
+    public void princ()
+    {
+        System.out.println("Principle diagonal elements:");
+        for(int i=0;i<row;i++)
+        {
+            System.out.print(m[i][i]+" ");
+        }
+        System.out.println();
+    }
+    public void nprinc()
+    {
+        System.out.println("NON Principle diagonal elements:");
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                if(i!=j)
+
+                System.out.print(m[i][j]+" ");
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) throws InterruptedException
+    {
+        Matrixthread l=new Matrixthread(3, 3);
+        Thread t1=new Thread(()->l.display());
+        Thread t2=new Thread(()->l.transpose());
+        Thread t3=new Thread(()->l.maxinmat());
+        Thread t4=new Thread(()->l.princ());
+        Thread t5=new Thread(()->l.nprinc());
+        t1.start();
+        Thread.sleep(100);
+        t2.start();
+        Thread.sleep(100);
+        t3.start();
+        Thread.sleep(100);
+        t4.start();
+        Thread.sleep(100);
+        t5.start();
+
+    }
+}
