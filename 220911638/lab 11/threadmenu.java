2380fb0f4690c9de4a27d8862b9ad26184f385ff
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2023-11-03 09:18:24 +0530

    Update threadmenu.java

--- 220911638/lab 11/threadmenu.java
+++ 220911638/lab 11/threadmenu.java
@@ -1,40 +1,47 @@
-import java.util.*;
+import java.util.Scanner;
 
-
-class Runn implements Runnable{
-    public void run(){
-        System.out.println("Thread started using Runnable");
+class RunnableDemo implements Runnable {
+    public void run() {
+        System.out.println("Thread created using Runnable interface.");
     }
 }
 
-class Thr extends Thread{
-    public void run(){
-        System.out.println("Thread running using Threads");
+class ThreadDemo2 extends Thread {
+    public void run() {
+        System.out.println("Thread created by inheriting Thread class.");
     }
 }
 
-class threadmenu{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        
-        do{
-            System.out.println("1.Runnable\n2.Thread\n3.exit");
-            n=sc.nextInt();
-            switch(n)
-            {
+public class ThreadDemo {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("1. Create thread using Runnable interface");
+            System.out.println("2. Create thread by inheriting Thread class");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
                 case 1:
-                    Runn r=new Runn();
-                    Thread t=new Thread(r);
-                    t.start();
+                    RunnableDemo runnableDemo = new RunnableDemo();
+                    Thread thread1 = new Thread(runnableDemo);
+                    thread1.start();
+                    Thread.sleep(100);
                     break;
                 case 2:
-                    Thr t2=new Thr();
-                    t2.start();
+                    ThreadDemo2 threadDemo2 = new ThreadDemo2();
+                    threadDemo2.start();
+                    Thread.sleep(100);
                     break;
                 case 3:
+                    System.out.println("Exiting program...");
                     System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
-        }while(true);
+        } while (true);
     }
 }
+
